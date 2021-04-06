f3dc2fa70b643d0e44d0272ef72e1f7e24c01125
Mingxun Wang <mwang87@gmail.com>
2021-04-06 13:18:29 -0700

    removing dependency

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import dash_html_components as html
 import dash_table
 import plotly.express as px
 import plotly.graph_objects as go 
-import dash_daq as daq
 from dash.dependencies import Input, Output, State
 import os
 from zipfile import ZipFile
